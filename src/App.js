9a942dd8bb58bfd0468da4feb55fb05dce14afc3
Mustakmia <musyafijur52@gmail.com>
2021-10-03 22:30:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { NotFound } from 'http-errors';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import About from './component/About/About';
+import Friends from './component/Friends/Friends';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/friends">
+            <Friends></Friends>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>kkkkkkkkkkkkkkkkk</h1>
+        </div>
+    );
+};
+
+export default Footer;
